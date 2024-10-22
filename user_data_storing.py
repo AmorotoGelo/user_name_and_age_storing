4e3b962ebe7230338db3aa40d0d3b22f4227746e
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-10-22 12:12:21 +0800

    Initial Pseudocode

---
+++ user_data_storing.py
@@ -0,0 +1,28 @@
+# Create an empty list to store data
+
+# Define a valid name to only accept alphabet with at least 1 character
+
+# Define a valid age to only accept positive integer ranging 0-120
+
+# Create a loop that asks user for name and age
+
+# Use try and except to validate the datas
+
+# Ask user to input a name
+
+# Raise an error if the name is invalid, ask user to input an alphabet
+
+# Ask user to input age
+
+# Raise an error if the name is invalid, ask user to input positive integer from 0-120
+
+# Store data in list
+
+#Ask user if they want to input another data
+
+# Break if the user input no
+
+# Print error message if the user input neither yes nor no
+
+
+
